365d8e4561d89762f50df3479da77bdea79f2be2
JongminLee <leejongmin@i2max.co.kr>
2024-04-01 14:03:48 +0900

    -

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,3 @@
-import React from 'react'
-// React 컴포넌트를 사용하기 위해 React를 import 합니다.
-
-// About 페이지를 위한 함수형 컴포넌트를 정의합니다.
 const About = () => {
     return (
         <div>
@@ -11,5 +7,4 @@ const About = () => {
     );
 };
 
-// 다른 파일에서 About 컴포넌트를 사용할 수 있도록 export 합니다.
 export default About;
