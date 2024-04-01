cfae6aaa37869d5db56bc4896a418e99e1a05874
JongminLee <leejongmin@i2max.co.kr>
2024-04-01 13:17:08 +0900

    -

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>Welcome to My Next.js App!</h1>
-            {/* 다른 컴포넌트들과 함께 초기 화면 내용을 추가합니다. */}
-        </div>
-    );
-}
