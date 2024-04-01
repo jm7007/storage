5067df9ffee1cec42fee70f1cf2cda54b4cb773f
JongminLee <leejongmin@i2max.co.kr>
2024-04-01 13:49:05 +0900

    -

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+// React 컴포넌트를 사용하기 위해 React를 import 합니다.
+
+// About 페이지를 위한 함수형 컴포넌트를 정의합니다.
+const About = () => {
+    return (
+        <div>
+            <h1>소개 페이지</h1>
+            <p>이 페이지는 Next.js를 사용하여 구현된 간단한 소개 페이지입니다.</p>
+        </div>
+    );
+};
+
+// 다른 파일에서 About 컴포넌트를 사용할 수 있도록 export 합니다.
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
     return (
-        <h2>Hello World!!</h2>
+        <div>
+            <h2>Hello World!!</h2>
+            <p> Click <Link href="/about"><a>this</a></Link> to move in About.js</p>
+        </div>
     );
 }
