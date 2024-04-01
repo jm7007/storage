f6b909f817fe32e9630a169356355e986fca7b4b
JongminLee <leejongmin@i2max.co.kr>
2024-04-01 14:06:02 +0900

    -

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div>
             <h2>Hello World!!</h2>
-            <p> Click <Link href="/about"><a>this</a></Link> to move in About.js</p>
+            <p> Click <Link href="/about">this</Link> to move in About.js</p>
         </div>
     );
 }
